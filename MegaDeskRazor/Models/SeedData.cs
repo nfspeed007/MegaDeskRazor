cf4e772dafb42a65dd61e114e55059f25ca27b32
nfspeed007 <isr15001@byui.edu>
2021-02-27 13:20:50 -0700

    Seed the Database

---
+++ MegaDeskRazor/Models/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using MegaDeskRazor.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDeskRazor.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MegaDeskRazorContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MegaDeskRazorContext>>()))
+            {
+                // Look for any Quotes.
+                if (context.Desk.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Desk.AddRange(
+                    new Desk
+                    {
+                        customerName = "Bill Gates",
+                        width = 30.00,
+                        depth = 30.00,
+                        numberOfDrawers = 2,
+                        price = 1025.00M,
+                        surfaceMaterial = "Rosewood",
+                        rushOrder = "7",
+                        DateAdded = DateTime.Parse("2021-02-25")
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
